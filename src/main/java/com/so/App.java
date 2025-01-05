a1cb3d18506b2b14dbe7604a1d71bb47dd0d4fec
Wilson <wilsonescorpiao7@gmail.com>
2025-01-05 12:24:36 -0300

    Projeto 2 algoritmo RoundRobin e SJF

--- src/main/java/com/so/App.java
+++ src/main/java/com/so/App.java
@@ -1,6 +1,7 @@
 package com.so;
 
 import java.io.IOException;
+import java.net.URL;//add
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,17 +16,14 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        String caminho = "/com/so/telas/MainMenu.fxml";
         // Carrega a tela principal
-        try{
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("telas/MainMenu.fxml"));
-            Parent root = loader.load();
-            Scene tela = new Scene(root);
-            stage.setTitle("Escalonador");
-            stage.setScene(tela);
-            stage.show();
-        }catch (Exception e){
-            System.out.println(" Erro ============================" + e.toString());
-        }
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(caminho));
+        Parent root = loader.load();
+        Scene tela = new Scene(root);
+        stage.setTitle("Escalonador");
+        stage.setScene(tela);
+        stage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/so/telas/MainMenu.java
+++ src/main/java/com/so/telas/MainMenu.java
@@ -18,40 +18,43 @@ public class MainMenu {
 
     @FXML
     private Button rr;
-    
+
     @FXML
     private Button ssjf;
 
     // Abre a tela de configuração do algoritmo de round robin
     @FXML
     void rrConfig(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("RRConfig.fxml"));
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        
+            Parent root = FXMLLoader.load(getClass().getResource("RRConfig.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        
     }
 
     // Abre a tela de resultados gerais
     @FXML
     void showResults(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("showResult.fxml"));
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(loader.load());
-        stage.setScene(scene);
-        showResult controller = loader.getController();
-        controller.initialLoad();
-        stage.show();
+        
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("showResult.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(loader.load());
+            stage.setScene(scene);
+            showResult controller = loader.getController();
+            controller.initialLoad();
+            stage.show();
     }
 
     // Abre a tela de configuração do algoritmo de shortest job first
     @FXML
     void sjfConfig(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("sjfConfig.fxml"));
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+            Parent root = FXMLLoader.load(getClass().getResource("sjfConfig.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
     }
-    
+
 }
